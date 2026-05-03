9151638c91c744312a06f9cb3f8095851f1253c9
MahmoudRomman <mahmoud.sayyedahmed900@gmail.com>
2026-05-03 22:54:24 +0300

    feat: adding the auth views login - logout - profile - update profile

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register_user
+from .views import register_user, UserProfileView
 
 urlpatterns = [
-    path('register/', register_user, name='register'),
+    path('users/register/', register_user, name='register'),
+    path('users/me/', UserProfileView.as_view(), name='user_profile'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,10 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes, APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserProfileSerializer
+from django.core.exceptions import ObjectDoesNotExist
+from .models import User
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -15,3 +17,24 @@ def register_user(request):
             status=status.HTTP_201_CREATED
         )
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def patch(self, request):
+        user = request.user
+        serializer = UserProfileSerializer(user, data=request.data, partial=True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request):
+        return self.patch(request)
